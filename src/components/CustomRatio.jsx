49997cdbfba463debdff8e66d6d6695088924d4e
Gaston <gaston10.c@hotmail.com>
2023-09-07 19:20:30 -0300

    base de los pasos

---
+++ src/components/CustomRatio.jsx
@@ -0,0 +1,44 @@
+import { RadioGroup, Radio, useRadio, VisuallyHidden, cn } from "@nextui-org/react";
+
+
+const CustomRadio = (props) => {
+    const {
+        Component,
+        children,
+        isSelected,
+        description,
+        getBaseProps,
+        getWrapperProps,
+        getInputProps,
+        getLabelProps,
+        getLabelWrapperProps,
+        getControlProps,
+    } = useRadio(props);
+
+    return (
+        // max-w-[300px]
+        <Component
+            {...getBaseProps()}
+            className={cn(
+                "group inline-flex items-center hover:opacity-70 active:opacity-50 justify-between flex-row-reverse tap-highlight-transparent",
+                "w-full cursor-pointer border-2 border-default rounded-lg gap-4 p-4",
+                "data-[selected=true]:border-primary",
+            )}
+        >
+            <VisuallyHidden>
+                <input {...getInputProps()} />
+            </VisuallyHidden>
+            <span {...getWrapperProps()}>
+                <span {...getControlProps()} />
+            </span>
+            <div {...getLabelWrapperProps()}>
+                {children && <span {...getLabelProps()}>{children}</span>}
+                {description && (
+                    <span className="text-small  text-foreground opacity-70">{description}</span>
+                )}
+            </div>
+        </Component>
+    );
+};
+
+export default CustomRadio;
